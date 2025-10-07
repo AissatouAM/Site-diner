ceca0df642087cdd4064484ad04964cc3a9840e5
Ayshaba <aissataba395@gmail.com>
2025-10-07 17:27:39 +0200

    changement dans  accueil2

--- accueil2/index.php
+++ accueil2/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/accueil2.css">
+    <link rel="stylesheet" href="../assets/css/accueil2.css">
     <title>Page d'accueil interne</title>
 </head>
 
@@ -13,8 +13,8 @@
         <div class="menu">
             <h3>Coder's dinner 2026-Election Roi & Reine</h3>
             <nav>
-                <a href="deconnexion.html">Déconnexion</a>
-                <a href="profil.html">Profil</a>
+                <a href="../accueil1/index.php">Déconnexion</a>
+                <a href="">Profil</a>
             </nav>
         </div>
     </header>
@@ -23,8 +23,8 @@
             <h1>Bienvenue</h1>
             <p>Que souhaitez-vous faire ?</p>
         </div>
-        <a href="site_candidature.html" class="btn">Déposer ou gérer ma candidature</a>
-        <a href="vote.html" class="btn">Voter pour un candidat</a>
+        <a href="../candidature/index.php" class="btn">Déposer ou gérer ma candidature</a>
+        <a href="../vote/index.php" class="btn">Voter pour un candidat</a>
 
     </main>
 </body>
