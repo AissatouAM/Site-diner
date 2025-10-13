68c6d33349945c7a11b0e515c7df40f5ed1f4fbf
Ayshaba <aissataba395@gmail.com>
2025-10-13 11:37:36 +0200

    rectification

--- inscription/php/index.php
+++ inscription/php/index.php
@@ -17,7 +17,7 @@ if (empty($prenom) || empty($nom) || empty($telephone) || empty($password) || em
 
 // 2. Vérifier que les mots de passe correspondent
 if ($password !== $confirm) {
-   $_SESSION['erreur_mdp'] = "Les mots de passe ne correspondent pas."
+   $_SESSION['erreur_mdp'] = "Les mots de passe ne correspondent pas." ;
    header("Location: ../html/index.php")
     exit();
 }
