516a2a9d1b9f1d0f8d894030a9f448c1af78fea9
Ayshaba <aissataba395@gmail.com>
2025-10-13 11:52:14 +0200

    rectification

--- inscription/html/index.php
+++ inscription/html/index.php
@@ -13,13 +13,9 @@
     <div class="centre"></div>
     <div class="container">
         <h2>Inscription</h2>
+
         <?php
-        // Affiche un message simple si la confirmation du mot de passe a échoué
         session_start();
-        if (isset($_SESSION['erreur_mdp'])) {
-            echo "<p style='color: red; text-align:center;'>" . $_SESSION['erreur_mdp'] . "</p>";
-            unset($_SESSION['erreur_mdp']);
-        }
         $confirm_error = isset($_GET['confirm_error']) && $_GET['confirm_error'] == 1;
         $pref_prenom = isset($_GET['prenom']) ? htmlspecialchars($_GET['prenom']) : '';
         $pref_nom = isset($_GET['nom']) ? htmlspecialchars($_GET['nom']) : '';
@@ -46,18 +42,15 @@
             <input type="password" name="password" placeholder="Mot de passe" required/>
             <input type="password" name="confirm_password" placeholder="Confirmer le mot de passe" required />
             
-
-            <!--Affichage du message-->
-            <?php if ($confirm_error): ?>
-                <div style="color:red;margin-top:6px;">Les mot de passe ne correspondent pas</div>
-            <?php endif; ?>
-
-
             <button type="submit" class="bouton">S'inscrire</button>
-            
-            
+             
             <p class="message">Déjà inscrit? <a href="../../connexion/html/index.php">Se connecter</a></p>
         </form>
+        <?php
+        if (isset($_SESSION['erreur_mdp'])) {
+            echo "<p style='color: red; text-align:center;'>" . $_SESSION['erreur_mdp'] . "</p>";
+            unset($_SESSION['erreur_mdp']);
+        }
     </div>
 </body>
 
