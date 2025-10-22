ec6ad1f62ba3c11f7733b9bfcde44d51ec343229
Ayshaba <aissataba395@gmail.com>
2025-10-22 22:33:53 +0200

    ajout email dans la base de donnee

--- db/site_diner.sql
+++ db/site_diner.sql
@@ -13,6 +13,7 @@ CREATE TABLE `utilisateurs` (
     `id_utilisateur` INT AUTO_INCREMENT PRIMARY KEY, 
     `nom` VARCHAR(100) NOT NULL,
     `prenom` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
     `telephone` VARCHAR(30) NOT NULL UNIQUE, 
     `mot_de_passe` VARCHAR(255) NOT NULL, 
     `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP 
