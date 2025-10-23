9aadafc19660e706e1b19392ff9147b56d4f0a50
Ayshaba <aissataba395@gmail.com>
2025-10-23 13:10:14 +0200

    ajout

--- reinitialisation/index.php
+++ reinitialisation/index.php
@@ -12,6 +12,7 @@ unset($_SESSION['message'], $_SESSION['type']);
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
 <title>Mot de passe oublié</title>
 <link rel="stylesheet" href="../assets/css/reinitialisation.css">
 <style>
